4fe3631047a1b9fc8aa6b034f77aa8a1b7ab86fc
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-07-07 05:55:19 +0300

    fix req.query parametrs add search in getProducts

--- controllers/productsController/productsController.js
+++ controllers/productsController/productsController.js
@@ -4,7 +4,7 @@ const { HttpStatusCode } = require('../../libs');
 class ProductsController {
   async getProducts(req, res, next) {
     try {
-      const products = await repositoryProducts.getProductsQuery(req.query);
+      const products = await repositoryProducts.getProductsQuery(req.query.search);
       res.json({
         status: 'success',
         code: HttpStatusCode.OK,
