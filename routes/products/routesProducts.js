1795c749a5d309a689941d5f0f7906a4f2ac068e
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-07-09 14:27:30 +0300

    fix rout getproducts no guard

--- routes/products/routesProducts.js
+++ routes/products/routesProducts.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const { productsController } = require('../../controllers');
-const { guard, validationWraperSchema} = require('../../middlewares');
-const {productOnDayJoiSchema } = require('../../models/productOnDayModel');
+const { guard, validationWraperSchema } = require('../../middlewares');
+const { productOnDayJoiSchema } = require('../../models/productOnDayModel');
 
 const router = new Router();
 
-router.get('/', guard, productsController.getProducts);
+router.get('/', productsController.getProducts);
 router.post('/', guard, validationWraperSchema(productOnDayJoiSchema), productsController.addProductOnDay);
 router.delete('/:id', guard, productsController.removeProductOnDay);
 router.get('/:date', guard, productsController.allProductsPerDay);
-
 
 module.exports = router;
