447c9610cc7c3f974a231cc3adcf1d8c47935c28
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-07-10 16:49:37 +0300

    fix routes auth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const routesAuth = require('./auth/routesAuth');
 const routesProducts = require('./products/routesProducts');
 const routesUsers = require('./users/routesUsers');
 const routesDailyCalories = require('./dailyCalories/routesDailyCalories');
-const routesAuthGoogle = require('./authGoogle/routesAuthGoogle');
 
-module.exports = { routesProducts, routesAuth, routesUsers, routesDailyCalories, routesAuthGoogle };
+module.exports = { routesProducts, routesAuth, routesUsers, routesDailyCalories };
