404013ee25ff7984a507e32e2999e64d71347613
Kretsul-Vitaliy <84912966+Kretsul-Vitaliy@users.noreply.github.com>
2022-07-13 09:32:36 +0300

    Update authController.js

--- controllers/authController/authController.js
+++ controllers/authController/authController.js
@@ -1,11 +1,8 @@
 const { Conflict, Unauthorized } = require('http-errors');
-
 const { authService } = require('../../services/auth');
 const { HttpStatusCode } = require('../../libs');
 
 const { EmailService, SenderNodemailer } = require('../../services/email');
-
-const { sessionService } = require('../../services/session');
 
 class AuthController {
   async signupUser(req, res, next) {
@@ -45,13 +42,17 @@ class AuthController {
       }
 
       const token = authService.getToken(authentificationUser);
+      const refreshToken = authService.getRefreshToken();
+
       await authService.setToken(authentificationUser.id, token);
+      await authService.setRefreshToken(authentificationUser.id, refreshToken);
 
       return res.status(HttpStatusCode.OK).json({
         status: 'success',
         code: HttpStatusCode.OK,
         data: {
           token,
+          refreshToken,
         },
       });
     } catch (error) {
@@ -62,6 +63,8 @@ class AuthController {
   async logoutUser(req, res, next) {
     try {
       await authService.setToken(req.user.id, null);
+      await authService.setRefreshToken(req.user.id, null);
+      
       res.status(HttpStatusCode.NO_CONTENT).json({
         status: 'success',
         code: HttpStatusCode.NO_CONTENT,
@@ -71,109 +74,6 @@ class AuthController {
     }
   }
 
-  async signupAuthGoogle(req, res, next) {
-    // const link = 'http://localhost:5000';
-    try {
-      const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-      const options = {
-        redirect_uri: `${process.env.BASE_URL}/api/v1/auth/google-redirect`,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        access_type: 'offline',
-        response_type: 'code',
-        promt: 'consent',
-        scope: [
-          'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile',
-        ].join(' '),
-      };
-      const qs = new URLSearchParams(options);
-      return res.redirect(`${rootUrl}?${qs.toString()}`);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async signupGoogleRedirect(req, res, next) {
-    // const linkFront = 'http://localhost:3000';
-
-    try {
-      // get the code from qs
-      const code = req.query.code;
-      const { id_token: idToken, access_token: accessingToken } = await authService.getGoogleOAuthToken(code);
-      // get user with tokens
-      const googleUser = await authService.getGoogleUser(idToken, accessingToken);
-      if (!googleUser.verified_email) {
-        return res.status(403).send('Google account is not verified');
-      }
-      // upsert the user
-      const isUserExist = await authService.isUserExist(googleUser.email);
-      if (isUserExist) {
-        const userInDataBase = await authService.getUserFromGoogle(googleUser.email);
-        const accessToken = await authService.getToken(userInDataBase);
-        await authService.setToken(userInDataBase.id, accessToken);
-        const sendUser = JSON.stringify({
-          name: userInDataBase.name,
-          email: userInDataBase.email,
-          avatarURL: userInDataBase.avatarURL,
-          token: accessToken,
-        });
-        return res.redirect(`${process.env.FRONTEND_URL}/google?user=${sendUser}`);
-      }
-      const user = await authService.createUserGoogle({
-        email: googleUser.email,
-        name: googleUser.name,
-        password: googleUser.id,
-        avatarURL: googleUser.picture,
-        isVerification: true,
-        verificationTokenEmail: null,
-      });
-
-      const accessToken = await authService.getToken(user);
-      await authService.setToken(user.id, accessToken);
-      const sendUser = JSON.stringify({
-        name: user.name,
-        email: user.email,
-        avatarURL: user.avatarURL,
-        token: accessToken,
-      });
-
-      // create a session
-      const session = await sessionService.createSession(user._id, req.get('user-agent') || '');
-
-      // create an access token
-      const accessTokenCookie = authService.signJwtAccess({ ...user, session: session._id });
-
-      // create a refresh token
-      const refreshTokenCookie = authService.signJwtRefresh({ ...user, session: session._id });
-
-      // set cookies
-      const accessTokenCookieOptions = {
-        maxAge: 900000, // 1 hours
-        httpOnly: true,
-        domain: 'localhost',
-        path: '/',
-        sameSite: 'lax',
-        secure: false,
-      };
-
-      const refreshTokenCookieOptions = {
-        ...accessTokenCookieOptions,
-        maxAge: 3.154e10, // 1 year
-      };
-
-      res.cookie('accessToken', accessTokenCookie, accessTokenCookieOptions);
-
-      res.cookie('refreshToken', refreshTokenCookie, refreshTokenCookieOptions);
-
-      // redirect back to client
-      return res.redirect(`${process.env.FRONTEND_URL}/google?user=${sendUser}`);
-    } catch (error) {
-      // next(error);
-      console.error(error);
-      return res.redirect(`${process.env.FRONTEND_URL}/google/error`);
-    }
-  }
-  
 }
 
 module.exports = new AuthController();
